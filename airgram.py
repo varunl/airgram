56b8e4aa487ebcd2019a78f5b1b031b45e1d9779
Varun Loiwal <varun@groupon.com>
2013-09-16 22:41:34 -0700

    Convert tabs to spaces

--- airgram.py
+++ airgram.py
@@ -4,82 +4,82 @@ import urllib2
 
 # A wrapper around the Airgram API, http://www.airgramapp.com/.
 # author: varun
-# author: Sidhant Gupta - authentication and defs for airgram service 
+# author: Sidhant Gupta - authentication and defs for airgram service
 
 
 class Airgram(object):
-	"""Wrapper around the Airgram API."""
+    """Wrapper around the Airgram API."""
 
-	BASE_URL = "https://api.airgramapp.com/1"
-	KEY = None
-	SECRET = None
+    BASE_URL = "https://api.airgramapp.com/1"
+    KEY = None
+    SECRET = None
 
-	def __init__(self, key=None, secret=None):
-		self.KEY = key
-		self.SECRET = secret
+    def __init__(self, key=None, secret=None):
+        self.KEY = key
+        self.SECRET = secret
 
-	def check_auth(self):
-		if self.KEY is None or self.SECRET is None:
-			raise Exception('Attempt to use function that requires a service key and secret!')
+    def check_auth(self):
+        if self.KEY is None or self.SECRET is None:
+            raise Exception('Attempt to use function that requires a service key and secret!')
 
-	def send(self, email, msg, url=None):
-		"""Send a push notification to specified user"""
+    def send(self, email, msg, url=None):
+        """Send a push notification to specified user"""
 
-		self.check_auth()
+        self.check_auth()
 
-		params = {
-		"email": email,
-		"msg": msg,
-		}
-		if url:
-			params["url"] = url
-		return self._post("/send", params, True)
+        params = {
+        "email": email,
+        "msg": msg,
+        }
+        if url:
+            params["url"] = url
+        return self._post("/send", params, True)
 
-	def subscribe(self, email):
-		"""Subscribe a new user to service"""
+    def subscribe(self, email):
+        """Subscribe a new user to service"""
 
-		self.check_auth()
-		params = {"email": email}
-		return self._post("/subscribe", params, True)
+        self.check_auth()
+        params = {"email": email}
+        return self._post("/subscribe", params, True)
 
-	def broadcast(self, msg, url=None):
-		"""Send a push notification to all subscribed users"""
+    def broadcast(self, msg, url=None):
+        """Send a push notification to all subscribed users"""
 
-		self.check_auth()
+        self.check_auth()
 
-		params = {"msg": msg}
-		if url:
-			params["url"] = url
-		return self._post("/broadcast", params, True)
+        params = {"msg": msg}
+        if url:
+            params["url"] = url
+        return self._post("/broadcast", params, True)
 
-	def send_as_guest(self, email, msg, url=None):
-		"""Send a push notification as a guest. No need to register your service.
+    def send_as_guest(self, email, msg, url=None):
+        """Send a push notification as a guest. No need to register your service.
 
-		Args:
-			email: The email to which you want to send the notification.
-			msg: The notification msg.
-			url: If the notification should direct you to a url.
+        Args:
+            email: The email to which you want to send the notification.
+            msg: The notification msg.
+            url: If the notification should direct you to a url.
 
-		Returns:
-			The response content.
-		"""
-		params = {
-				"email": email,
-				"msg": msg,
-		}
-		if url:
-			params["url"] = url
-		return self._post("/send_as_guest", params)
+        Returns:
+            The response content.
+        """
+        params = {
+                "email": email,
+                "msg": msg,
+        }
+        if url:
+            params["url"] = url
+        return self._post("/send_as_guest", params)
 
-	def _post(self, path, decoded_params, auth=None):
-		url = "%s%s" % (self.BASE_URL, path)
-		params = urlencode(decoded_params)
-		req = urllib2.Request(url, params)
-		
-		if auth:
-			base64string = encodestring('%s:%s' % (self.KEY, self.SECRET)).replace('\n', '')
-			req.add_header("Authorization", "Basic %s" % base64string)  
-		
-		rsp = urllib2.urlopen(req)
-		content = rsp.read()
-		return content
+    def _post(self, path, decoded_params, auth=None):
+        url = "%s%s" % (self.BASE_URL, path)
+        params = urlencode(decoded_params)
+        req = urllib2.Request(url, params)
+
+        if auth:
+            base64string = encodestring('%s:%s' % (self.KEY, self.SECRET)).replace('\n', '')
+            req.add_header("Authorization", "Basic %s" % base64string)
+
+        rsp = urllib2.urlopen(req)
+        content = rsp.read()
+        return content
